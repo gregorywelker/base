b324d7403afeb70021ad60329a10cf030f40e7de
gregorywelker <gregory.welker@icloud.com>
2022-03-07 16:34:58 +0100

    Workflow test

--- train-controller/src/test/java/hu/bme/mit/train/controller/TrainControllerImplTest.java
+++ train-controller/src/test/java/hu/bme/mit/train/controller/TrainControllerImplTest.java
@@ -2,17 +2,10 @@ package hu.bme.mit.train.controller;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
-import hu.bme.mit.train.interfaces.TrainController;
-import hu.bme.mit.train.interfaces.TrainSensor;
-import hu.bme.mit.train.interfaces.TrainUser;
-import hu.bme.mit.train.system.TrainSystem;
 import static org.mockito.Mockito.*;
 
-public class TrainSensorTest {
+public class TrainControllerImplTest {
 
     @Test
     public void IsActiveTest() {
